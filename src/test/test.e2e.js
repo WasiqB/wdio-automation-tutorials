0b4fee01da8ae096ef03a9746776a552d8369fc4
WasiqB <wasbhamla2005@gmail.com>
2023-07-29 20:50:42 +0530

    Initial commit

---
+++ src/test/test.e2e.js
@@ -0,0 +1,17 @@
+/**
+ * test with page objects
+ */
+import LoginPage from "../pages/login.page.js";
+import SecurePage from "../pages/secure.page.js";
+
+describe("My Login application", () => {
+  it("should login with valid credentials", async () => {
+    await LoginPage.open();
+
+    await LoginPage.login("tomsmith", "SuperSecretPassword!");
+    await expect(SecurePage.flashAlert).toBeExisting();
+    await expect(SecurePage.flashAlert).toHaveTextContaining(
+      "You logged into a secure area!"
+    );
+  });
+});
